c1e1cc6c15ed729230f1339607bb766f4f889730
Gao Yuan <ygao@thoughtworks.com>
2014-07-22 00:09:25 +0800

    get back the correct version...

--- exercise1/FizzBuzz/test/FizzBuzz_spec.rb
+++ exercise1/FizzBuzz/test/FizzBuzz_spec.rb
@@ -1,26 +1,22 @@
 require "rspec"
 require_relative "../src/FizzBuzzPrinter.rb"
 
-describe "FizzBuzzPrinter" do
+describe 'FizzBuzzPrinter' do
 
   def test_shouldPrintFizz
     assert_equal("Fizz",printFizzBuzz(3))
-    puts message
   end
 
   def test_shouldPrintFizz
     assert_equal("Buzz",printFizzBuzz(5))
-    puts message
   end
 
   def test_shouldPrintFizz
     assert_equal("FizzBuzz",printFizzBuzz(15))
-    puts message
   end
 
   def test_shouldPrintFizz
     assert_equal("7",printFizzBuzz(7))
-    puts message
   end
 
 end
