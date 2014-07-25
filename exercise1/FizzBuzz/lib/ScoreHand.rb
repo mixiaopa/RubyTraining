03d60a99e53ba1db20f2bf44c21864c39474dbf2
Gao Yuan <ygao@thoughtworks.com>
2014-07-25 19:56:18 +0800

    Finish ScoreHand

--- exercise1/FizzBuzz/lib/ScoreHand.rb
+++ exercise1/FizzBuzz/lib/ScoreHand.rb
@@ -2,17 +2,23 @@ def scoreHand str
   countA = str.select{|p| p == "A"}.size
 
   result = str.push(0).reject {|p| p == "A"}.reduce{|sum, p|
-    if p =~ /[JQK]/
-      p=10
+    if p =~ /[JQK]/ || sum =~ /[JQK]/
+      if p =~ /[JQK]/
+        p=10
+      end
+      if sum =~ /[JQK]/
+        sum = 10
+      end
     end
     sum.to_i + p.to_i
     }
-  puts result
-  if result > 10
-    result + 1*countA
-  else
-    result + 11*countA
-  end
-end
 
-# scoreHand(["A"])
+  if countA >= 1
+      result += countA * 11
+      if result > 21
+        result -= 10 * countA
+      end
+  end
+
+  return result
+end
--- exercise1/FizzBuzz/spec/ScoreHand_spec.rb
+++ exercise1/FizzBuzz/spec/ScoreHand_spec.rb
@@ -4,16 +4,23 @@ require_relative "../lib/ScoreHand.rb"
 describe "ScoreHand" do
 
     it "test_shouldReturnElevenWhenGetA" do
-      11.should eq(scoreHand(["A"]))
+      scoreHand(["A"]).should eq(11)
     end
 
     it "test_shouldReturn21WhenGetAJ" do
-      21.should eq(scoreHand(["A", "J"]))
+      scoreHand(["A", "J"]).should eq(21)
     end
 
     it "test_shouldReturn12WhenGetA10A" do
-      12.should eq(scoreHand(["A", "10", "A"]))
+      scoreHand(["A", "10", "A"]).should eq(12)
     end
 
+    it "test_shouldReturn15WhenGet537" do
+      scoreHand(["5", "3", "7"]).should eq(15)
+    end
+
+    it "test_shouldReturn25WhenGet5432AK" do
+      scoreHand(["5", "4", "3", "2", "A", "K"]).should eq(25)
+    end
 
 end
